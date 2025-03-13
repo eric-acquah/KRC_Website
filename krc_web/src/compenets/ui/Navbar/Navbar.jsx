37965320508ef884af8d92a6198e9fee7e301157
Eric Acquah <ericacquah1720@gmail.com>
2025-03-13 15:29:25 +0000

    Completed navbar component

--- krc_web/src/compenets/ui/Navbar/Navbar.jsx
+++ krc_web/src/compenets/ui/Navbar/Navbar.jsx
@@ -1,11 +1,12 @@
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 export default function Navbar() {
     return (
-        <nav className='navbar navbar-expand-lg bg-body-tertiary'>
+        <nav className='navbar navbar-expand-lg '>
             <div className='container'>
                 <a className='navbar-brand' href="#">
-                    <img src="/src/assets/krc-logo.png" alt="logo" width="150" className='logo' />
+                    <img src="/src/assets/krc-logo-1.png" alt="logo" width="150" className='logo' />
                 </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className='navbar-toggler-icon'></span>
@@ -13,10 +14,10 @@ export default function Navbar() {
                 <div className="collapse navbar-collapse" id="navbarContent">
                     <ul className='navbar-nav ms-auto mb-2 mb-lg-0'>
                         <li className='nav-item'>
-                            <a className='nav-link' href="#">Home</a>
+                            <Link className='nav-link' to="/">Home</Link>
                         </li>
                         <li className='nav-item'>
-                            <a className='nav-link' href="#">About</a>
+                            <Link className='nav-link' to="/about-us">About Us</Link>
                         </li>
                         <li className='nav-item'>
                             <a className='nav-link' href="#">Portfolio</a>
